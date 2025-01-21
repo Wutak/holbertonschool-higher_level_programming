29ed5efff6fb01c0fbf6e96044e3b660988d6bd0
julien <julienvillessot@gmail.com>
2025-01-21 13:07:26 +0000

    fix 3-6

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def new_in_line(my_list, idx, element):
-    cp = my_list
-    if idx < 0:
-        return (cp)
-    elif idx > len(my_list)
-            return (cp)
+    cp = my_list[:]
+    if idx < 0 or idx > len(my_list)
+        return cp
 
     copy[idx] = element
-    return (cp)
+    return cp
--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -2,4 +2,4 @@
 def no_c(my_string):
     new_string = my_string
     new_string = ''.join(char for char in my_string if char != 'c' and char != 'C')
-    print(new_string)
+    return new_string
