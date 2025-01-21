592d366bc85de6649c91babebac62b9076bc687f
julien <julienvillessot@gmail.com>
2025-01-21 13:13:08 +0000

    fix 4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def new_in_line(my_list, idx, element):
-    cp = my_list[:]
-    if idx < 0 or idx > len(my_list)
-        return cp
+    new_list = my_list[:]
+    if idx < 0 or idx >= len(my_list)
+        return new_list
 
-    cp[idx] = element
-    return cp
+    new_list[idx] = element
+    return new_list
