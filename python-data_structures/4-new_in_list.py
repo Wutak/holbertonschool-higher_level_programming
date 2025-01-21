068b2ec4928f8b68481b5c9e4879cf9700314fe0
julien <julienvillessot@gmail.com>
2025-01-21 10:39:41 +0000

    save

---
+++ python-data_structures/4-new_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def new_in_line(my_list, idx, element):
+    cp = my_list
+    if idx < 0:
+        return (cp)
+    elif idx > len(my_list)
+            return (cp)
+
+    copy[idx] = element
+    return (cp)
