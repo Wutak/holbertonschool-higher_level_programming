400ce8313eada41dea3ce3ea6b34851e1479d6d0
julien <julienvillessot@gmail.com>
2025-01-24 08:49:57 +0000

    fix all

--- python-more_data_structures/7-update_dictionary.py
+++ python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def update_dicitonary(a_dictionary, key, value):
-    for k, v in a_dictionary.item():
-        if k == key:
-            a_dictionay[k] = value
-            break
-        else:
-            a_dictionary[key] = value
-        return(a_dictionary)
+    a_dictionary[key] = value
+    return(a_dictionary)
