711b675dc5ce22c9fc069877485f1abe38761d61
julien <julienvillessot@gmail.com>
2025-01-28 10:22:29 +0000

    2

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -5,10 +5,9 @@
 class Square:
     """size must be an int and greater than 0"""
 
-
     def __init__(self, size=0):
         """optional initialization with size, verify value and type"""
-        if type(size) is not int:
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
