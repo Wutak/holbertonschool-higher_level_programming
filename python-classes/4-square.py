1336f3dd3ef9402673de955b80d28fc770188432
julien <julienvillessot@gmail.com>
2025-01-29 13:48:02 +0000

    4-6

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -6,13 +6,13 @@ class Square:
     """defines a square by its size"""
 
     def __init__(self, size=0):
-        """optional initialization with private attrivute size, an int >= 0"""
+        """optional initialization with private attribute size, an int >= 0"""
         self.size = size
 
     @property 
     def size(self):
         """method to retrieve size value"""
-        return self.size
+        return self._size
 
     @size.setter
     def size(self, value):
--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -6,13 +6,13 @@ class Square:
     """defines a square by its size"""
 
     def __init__(self, size=0):
-        """optional initialization with private attrivute size, an int >= 0"""
+        """optional initialization with private attribute size, an int >= 0"""
         self.size = size
 
     @property
     def size(self):
         """method to retrieve size value"""
-        return self.size
+        return self._size
 
     @size.setter
     def size(self, value):
@@ -22,7 +22,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         else:
-            self.size = value
+            self._size = value
 
     def area(self):
         """returns area of the square"""
