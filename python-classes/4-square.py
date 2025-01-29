283d6bbc4822001135e15ebcd24ecbfddc7cfeb4
julien <julienvillessot@gmail.com>
2025-01-29 13:42:27 +0000

    4

--- python-classes/4-square.py
+++ python-classes/4-square.py
@@ -7,7 +7,7 @@ class Square:
 
     def __init__(self, size=0):
         """optional initialization with private attrivute size, an int >= 0"""
-        self._size = size
+        self.size = size
 
     @property 
     def size(self):
