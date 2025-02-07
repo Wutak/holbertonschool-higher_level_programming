e9a473af73935d056b21748a7fb7013bf1fa2d15
julien <julienvillessot@gmail.com>
2025-02-07 14:48:11 +0000

    2-5

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,25 +1,25 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+
+
 class VerboseList(list):
 
     def append(self, item):
-        print(f"Added [{item]} to the list.")
+        print(f"Added [{item}] to the list.")
         super().append(item)
 
-    def extend(self, iterable):
-        count = len(iterable)
-        print(f"Extended the list with [{count}] items.")
-        super().extend(iterable)
+    def extend(self, item):
+        print(f"Extended the list with [{len(item)}] items.")
+        super().extend(item)
 
     def remove(self, item):
         if item not in self:
             raise ValueError("Can't find this value in this instance")
         print(f"Removed [{item}] from the list.")
         super().remove(item)
-        
-    def pop(self, index=-1):
-        
+
+    def pop(self, item=-1):
         if self[item] is None:
             raise IndexError("Index does not exist")
-        print(f"Popped[{item}] from the list.")
-        retour =  super().pop(item)
+        print(f"Popped [{self[item]}] from the list.")
+        retour = super().pop(item)
         return retour
