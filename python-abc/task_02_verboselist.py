2cddd00ea7251b2b24af73785514af6445b11f7f
julien <julienvillessot@gmail.com>
2025-02-07 14:46:32 +0000

    2-5

--- python-abc/task_02_verboselist.py
+++ python-abc/task_02_verboselist.py
@@ -1,26 +1,25 @@
 #!/usr/bin/python3
 class VerboseList(list):
+
     def append(self, item):
-        super().append(item)
         print(f"Added [{item]} to the list.")
+        super().append(item)
 
     def extend(self, iterable):
         count = len(iterable)
-        super().extend(iterable)
         print(f"Extended the list with [{count}] items.")
+        super().extend(iterable)
 
     def remove(self, item):
-        if item in self:
-            print(f"Removed [{item}] from the list.")
-            super().remove(item)
-        else:
-            print(f"Item [{item}] not found in the list.")
-
+        if item not in self:
+            raise ValueError("Can't find this value in this instance")
+        print(f"Removed [{item}] from the list.")
+        super().remove(item)
+        
     def pop(self, index=-1):
-        item = self[index] if len(self) > 0 else None
-        if item is not None:
-            print(f"Popped[{item}] from the list.")
-            return super().pop(index)
-        else:
-            print("Cannot  pop from an empty list.")
-            return None
+        
+        if self[item] is None:
+            raise IndexError("Index does not exist")
+        print(f"Popped[{item}] from the list.")
+        retour =  super().pop(item)
+        return retour
---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+class CountedIterator:
+
+    def __init__(self, iterable):
+        self.iterator = iter(iterable)
+        self.counter = 0
+
+    def get_count(self):
+        return self.counter
+
+    def __next__(self):
+            item = next(self.iterator)
+            self.counter += 1
+            return item
+
+    def __iter__(self):
+        return self
