1e94882ab3f6dd00e62a86991b90047d66947f1e
julien <julienvillessot@gmail.com>
2025-02-20 09:29:44 +0000

    push 2

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -7,18 +7,20 @@ import csv
 def fetch_and_print_posts():
     """request"""
 
-    response = requests.get(https://jsonplaceholder.typicode.com/posts)
-    print(f"Status Code : {}".format(response.status_code))
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    print("Status Code:" response.status_code)
     if response.status_code == 200:
         posts = response.json()
         for post in posts:
             print(post["title"])
+        else:
+            print("Error fecthing data")
 
 
 def fetch_and_save_posts():
     """request"""
 
-    response = resquests.get(https://jsonplaceholder.typicode.com/posts)
+    response = resquests.get("https://jsonplaceholder.typicode.com/posts")
     if response.status_code == 200:
         posts = response.json()
 
