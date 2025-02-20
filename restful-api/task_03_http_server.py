dfa1802fa5b19b771d4bdcd4027683a506a2f821
julien <julienvillessot@gmail.com>
2025-02-20 10:19:19 +0000

    push 3

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -6,19 +6,16 @@ import json
 
 
 class http_SubClass(http.server.BaseHTTPRequestHandler):
-    """ge√re les reqe√tes tp de type GET"""
-
+    """gere les requetes http de type GET"""
     def do_GET(self):
         """http server"""
         if self.path == "/":
-            """Accueil"""
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"Hello, this is a simple API!")
 
         elif self.path == "/data":
-            """Donn√es JSON. Renvoie un objet json"""
             dataset = {"name": "John", "age": 30, "city": "New York"}
             self.send_response(200)
             self.send_header("Content-type", "application/json")
@@ -26,7 +23,6 @@ class http_SubClass(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(dataset).encode("utf-8"))
 
         elif self.path == "/info":
-            """Informations sur l'API. Renvoie une description json"""
             dataset = {"version": "1.0", "description":
                        "A simple API built with http.server"}
             self.send_response(200)
@@ -35,20 +31,17 @@ class http_SubClass(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps(dataset).encode("utf-8"))
 
         elif self.path == "/status":
-            """Statut. Le serveur re√pond ok"""
             self.send_response(200)
             self.send_header("Content-type", "text/plain")
             self.end_headers()
             self.wfile.write(b"OK")
         else:
-            """Gestion des erreurs"""
             self.send_response(404)
             self.send_header('Content-type', 'text/plain')
             self.end_headers()
             self.wfile.write(b"Endpoint not found")
 
 
-"""De√marrage du serveur"""
 PORT = 8000
 with socketserver.TCPServer(("", PORT), http_SubClass) as httpd:
     print("serving at port", PORT)
