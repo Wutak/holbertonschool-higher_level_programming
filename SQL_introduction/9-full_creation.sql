e2d4499634817ae6f14b907bdeed6f95b9354aa1
julien <julienvillessot@gmail.com>
2025-03-05 12:44:24 +0100

    push

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,7 +1,7 @@
 -- creates a second table in database
 CREATE TABLE IF NOT EXISTS second_table (
 	id INT PRIMARY KEY,
-	name VARCHAN(256),
+	name VARCHAR(256),
 	score INT
 );
 INSERT INTO second_table (id, name, score)
