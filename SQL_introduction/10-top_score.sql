5b3052d987762fba03006ae27d7b4f6b0a02b175
julien <julienvillessot@gmail.com>
2025-03-05 11:48:30 +0100

    push 10-16

--- SQL_introduction/10-top_score.sql
+++ SQL_introduction/10-top_score.sql
@@ -1,2 +1,2 @@
 -- lists all records of the table of the database
-SHOW * (
+SELECT score, name FROM second_table ORDER BY score DESC;
---
+++ SQL_introduction/11-best_score.sql
@@ -0,0 +1,2 @@
+-- lists all records with a score in the table
+SELECT score, name FROM second_table where score >= 10 ORDER BY score DESC;
