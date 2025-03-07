8955e620a03ced4a5f4a35303434eb73e90c7fc5
Wutak <julienvillessot@gmail.com>
2025-03-07 18:20:18 +0100

    Update 7-model_state_fetch_all.py

--- python-object_relational_mapping/7-model_state_fetch_all.py
+++ python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python3
 """class definition"""
-from sqlalchemy import Column, Integer, String, create_engine
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
-
-Base = declarative_base()
 
 class State(Base):
     """State class"""
     __tablename__ = 'states'
 
-    id = Column(integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, nullable=False, unique=True)
     name = Column(String(128), nullable=False)
