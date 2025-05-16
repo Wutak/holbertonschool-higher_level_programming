68d9af0cdd1b72159e5f0144c4c7db8679e2fc72
julien <julienvillessot@gmail.com>
2025-05-16 13:53:23 +0200

    fix

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def new_in_line(my_list, idx, element):
-    new_list = my_list[:]
     if idx < 0 or idx >= len(my_list):
-        return new_list
+        return list(my_list)
 
-    new_list = [i for i in my_list]
+    new_list = list(my_list)
     new_list[idx] = element
     return new_list
--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     if my_list:
-        new_list = [my_list[0]]
+        max_value = my_list[0]
         for i in my_list:
-            if i > new_list[0]:
-                new_line[0] = i
-            return(new_list[0])
-        return(None)
+            if i > max_value[0]:
+                max_value[0] = i
+        return max_value
+    return(None)
