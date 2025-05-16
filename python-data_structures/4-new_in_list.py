181375095117d83bdcd57587ab5f1465bef42c92
julien <julienvillessot@gmail.com>
2025-05-16 14:27:03 +0200

    fix

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -2,7 +2,6 @@
 def new_in_line(my_list, idx, element):
     if idx < 0 or idx >= len(my_list):
         return list(my_list)
-
     new_list = list(my_list)
     new_list[idx] = element
     return new_list
