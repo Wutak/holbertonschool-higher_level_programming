f5d2eb63dcc0e0a1d3151976a03f24c51dfb3d30
julien <julienvillessot@gmail.com>
2025-05-30 15:25:48 +0200

    push

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -46,5 +46,5 @@ class Rectangle(Shape):
 
 
 def shape_info(shape):
-    print(f: "Area: {shape.area()}")
-    print(f: "Perimeter: {shape.perimeter()}")
+    print(f"Area: {shape.area()}")
+    print(f"Perimeter: {shape.perimeter()}")
