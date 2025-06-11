44aecbbacc52ffef91a2cb56709f67a0ccc58144
julien <julienvillessot@gmail.com>
2025-06-11 15:49:19 +0200

    push

--- restful-api/task_02_requests.py
+++ restful-api/task_02_requests.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """request"""
 import requests
+import json
+import csv
 
 
 url = "https://jsonplaceholder.typicode.com/"
@@ -8,14 +10,22 @@ response = requests.get(url)
 
 def fetch_and_print_posts(response):
 
-    print("Status Code = 200")
+    print(f"Status Code: {response.status_code}")
     if response.status_code == 200:
         posts = response.json()
-    for post in posts:
-        print(f"Tile: {post['title']}")
+        for post in posts:
+            print(f"Tile: {post['title']}")
 
 def fetch_and_save_posts(response):
 
+    posts = response.json()
     for post in posts:
         if response.status_code == 200:
+            list_dict = [
+                    {"id": post["id"], "title": post["title"], "body": post["body"]}
+                    ]
+            with open("posts.csv", "w", newline='') as file:
+                writer = csv.DictWriter(file, fieldnames=["id", "title", "body"])
 
+                write.writeheader()
+                writer.writerows(list_dict)
---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""server"""
+import http.server
+import socketserver
+import json
+
+
+class http_SubClass(http.server.BaseHTTPRequestHandler):
+    """subclass"""
+
+    def do_GET(self):
+
+        if self.path == "/":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            dataset = {"name": "John", "age": 30, "city": "New York"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(dataset).encode("utf-8"))
+
+        elif self.path == "/info":
+            dataset = {"version": "1.0", "description":
+                       "A simple API built with http.server"}
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps(dataset).encode("utf-8"))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"OK")
