8f67d44c2cd9aa02802a3614342829b51c6b1884
julien <julienvillessot@gmail.com>
2025-07-11 16:02:14 +0200

    8

--- javascript-warm_up/8-square.js
+++ javascript-warm_up/8-square.js
@@ -4,9 +4,16 @@ const size = Number(process.argv[2]);
 if (!Number.isInteger(size)) {
   console.log('Missing size');
 } else {
-  const line = 'X'.repeat(size);
-  for (let i = 0; i < size; i++) {
+  let row = 0;
+  while (row < size) {
+    let col = 0;
+    let line = '';
+    while (col < size) {
+      line += 'X';
+      col++;
+    }
     console.log(line);
+    row++;
   }
 }
 
