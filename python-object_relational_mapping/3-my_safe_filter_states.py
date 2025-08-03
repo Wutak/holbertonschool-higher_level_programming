15a14b56fff1b2fc8e5f641054f5faedda36f363
julien <julienvillessot@gmail.com>
2025-08-03 19:05:30 +0200

    3

--- python-object_relational_mapping/3-my_safe_filter_states.py
+++ python-object_relational_mapping/3-my_safe_filter_states.py
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     cursor = db.cursor()
 
     cursor.execute(
-            "SELECT * FROM states WHERE name = %s "
+            "SELECT * FROM states WHERE name = '%s' "
             "ORDER BY id ASC;".format(state_name,))
 
     result = cursor.fetchall()
