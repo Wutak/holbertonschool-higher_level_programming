8ef9b3e9979cb6664556706b2ce0fcf7d38672f0
julien <julienvillessot@gmail.com>
2025-08-03 16:18:22 +0200

    1

--- python-inheritance/1-my_list.py
+++ python-inheritance/1-my_list.py
@@ -3,13 +3,8 @@
 
 
 class Mylist(list):
-    """
-    mylist
-    """
+    """mylist"""
 
     def print_sorted(self):
         """print"""
-        if not isinstance(self, int):
-            raise AttributeError("instance must be a int")
-        else:
             print(sorted(self))
