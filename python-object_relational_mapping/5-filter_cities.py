6009c8807641dec5cee99efb3f8a4753144817e7
julien <julienvillessot@gmail.com>
2025-08-03 20:25:24 +0200

    5

--- python-object_relational_mapping/5-filter_cities.py
+++ python-object_relational_mapping/5-filter_cities.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     mysql_username = sys.argv[1]
     mysql_password = sys.argv[2]
     database_name = sys.argv[3]
-    state_id = sys.argv[4]
+    state_name = sys.argv[4]
     db = MySQLdb.connect(host='localhost',
                          user=mysql_username,
                          passwd=mysql_password,
