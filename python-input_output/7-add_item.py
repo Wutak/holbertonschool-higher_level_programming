9332f78f1c9d536e00be74bc3c9761e5d8ab1860
julien <julienvillessot@gmail.com>
2025-08-03 22:13:11 +0200

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -6,8 +6,9 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 
 if __name__ == "__main__":
-    items = load_from_json_file('add_to_json')
-except FileNotFoundError:
-    items = []
-items.extend(sys.argv[1:])
-save_to_json_file(items, 'add_to_json')
+    try:
+        items = load_from_json_file('add_to_json')
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, 'add_to_json')
