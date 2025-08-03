e7dabc39704d87f7c8d6f28f97a73697dcb3dde7
julien <julienvillessot@gmail.com>
2025-08-03 22:05:43 +0200

    7

--- python-input_output/7-add_item.py
+++ python-input_output/7-add_item.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """add item"""
-import sys import argv
+from sys import argv
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
